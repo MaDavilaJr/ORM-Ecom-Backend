7aca8c97bda0d48b9d3cf9f47be14e646e437bc5
Miguel Davila Jr <migueldavilajr@gmail.com>
2022-01-04 15:31:07 -0800

    added routes to category-routes.js, added functionality: find all/one category, create a new category, delete a category

--- models/index.js
+++ models/index.js
@@ -6,11 +6,36 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  // Define the table needed to store the foreign keys
+  foreignKey: 'category_id',
+  // Define an alias for when data is retrieved
+  as: 'product_category'
+});
+
 // Categories have many Products
-
+Category.belongsToMany(Product, {
+  through: {
+    model: ProductTag, 
+    unique: false
+  },
+  as: 'category_products'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Category, through: Product, as: 'category_id'}]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
